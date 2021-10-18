94417c5a4454c70be1679b21442ab7aa6ab5ee7c
Olav Vahtras <vahtras@kth.se>
2021-10-18 17:21:45 +0200

    open static script

---
+++ open_static.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import flask
+import random
+import pathlib
+
+
+root = str(pathlib.Path(__file__).parent.parent.resolve())
+stem = pathlib.Path(__file__).parent.parent.stem
+
+app = flask.Flask(
+    __name__,
+    static_folder=root,
+    static_url_path=f'/{stem}',
+    template_folder=root
+)
+
+
+@app.route('/')
+def index():
+    return flask.render_template('index.html')
+
+
+port = int(5000 + 5000*random.random())
+app.run(debug=True, port=port)
